b6abea3545e75403d84c5b9457914199f2f845fd
Yishak Wondim <it.yisak.wondim@gmail.com>
2022-09-03 09:24:10 +0300

    starting javscript

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 
+function getComputerChoice(){
+    let cards = ['rock','paper','scissor'];
+    let choice = _.sample(cards);
+    return choice;
+}
+
+
+const computerSelection = getComputerChoice();
+
+const playerChoice = document.querySelector('.selection[data-selection]=');
+
+
+function playRound(playerSelection, computerSelection) {
+    
+  }
+
+  function playerSelection(){
+
+}   
+
+function game(){
+
+}
+
